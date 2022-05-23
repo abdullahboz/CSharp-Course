5844c4b66836ab957453a2f98cc9e141fbb243d2
AbdullahBoz <abdullahboz@outlook.com>
2022-05-23 05:17:54 +0300

    İnterfaces
    interface temelleri

---
+++ Interfaces/ICustomerDal.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    /* Gerçek uygulamalarda SQL MYSQL ORACLE destekli bir yazılım yazıyorsanız.
+     * Interfaceleri her biri için implement etmeniz gerekir.
+     * Bu şekilde projemiz sadece sql'e yada başka bir veritabanına bağlı kalmıyor.
+     * ****************************************************************************
+     * Polimorpism ÇOK BİÇİMLİLİK ANLAMINA GELİR
+     * bir nesneyi farklı amaçlarla implement edip  bu implementasyonların tamamına veya belli bir kısmına ulaşmaktır.
+     * burada polimorphism yaptık IcustomerDal 'ı sql ve oracle biçiminde implement ettik.
+    */
+     interface ICustomerDal
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+    class SqlServerCustomerDal : ICustomerDal //implement ediyoruz.
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql delete");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql Update");
+        }
+    }
+    class OracleCustomerDal : ICustomerDal //implemen ediyoruz.
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle delete");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle update");
+        }
+    }
+    class MySqlCustomerDal : ICustomerDal //implemen ediyoruz.
+    {
+        public void Add()
+        {
+            Console.WriteLine("MySql added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("MySql delete");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("MySql update");
+        }
+    }
+
+    class CustomerManager
+    {
+        public void Add(ICustomerDal customerDal) // Parametre olarak veriyoruz.
+        {
+            customerDal.Add();
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        // I ile başlar
+        // En büyük kullanım amacı bir temel nes oluşturup bütün nesneleri ondan implement etmektir.
+        // İnterface soyut nesnedir. Tek başına anlam ifade etmez.
+        // interfaceler yazılırken metodun imzası ile yazılır.
+        /* Interfaceyi neden kullanıırız.
+         * Bir interface newlenemez çünkü tek başına anlamı yoktur. -> Iperson person = new Iperson(); Soyut nesneler newlenemez.
+         * 
+        */
+        static void Main(string[] args)
+        {
+            //InterfacesIntro();
+            //Demo();
+
+            // Senaryomuz şu sizin bir veriniz var ve bunu hem oracle veritabanına hem sql veritabanına yazmak istiyorsunuz.
+            // iki elemanlı bir customerDals tanımladık 1. SqlServerCustomerDdal 2. OracleCostumerDal
+            // Array olduğu için foreach ile gezeceğiz.
+            ICustomerDal[] customerDals = new ICustomerDal[3] { new SqlServerCustomerDal(),new OracleCustomerDal(),new MySqlCustomerDal() };
+            // burada hem oracle hem sql servere ekleme yaptık.
+            // burada ki güzellik şudur diyelim Mysql'e ekleme yapıcaz.
+            // Artık MYSQL'i de desteklemiş oluyoruz.
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+           
+            
+            Console.ReadLine(); 
+        }
+        private static void Demo()
+        {
+            // Burada amac uygulamanın bağımlılıklarını minimize etmektir.
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new SqlServerCustomerDal());
+            customerManager.Add(new OracleCustomerDal());
+
+        }
+        private static void InterfacesIntro()
+        {
+            PersonManager manager = new PersonManager();
+            //manager.Add(new Customer { Id = 1, FirtName = "Abdullah", LastName = "Boz", Address = "İstanbul" }); // ekleme çeşidi.
+            //manager.Add(new Student(){Id=1,FirtName="Abdullah",LastName="Boz",Departmant="Android Developer" }); //ekleme çeşidi.
+
+
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirtName = "Abdullah2",
+                LastName = "Boz",
+                Address = "İstanbul"
+            };
+
+
+            Student student = new Student()
+            {
+                Id = 1,
+                FirtName = "Abdullah",
+                LastName = "Boz",
+                Departmant = "Mobile Application Developer/Android developer"
+
+            };
+            manager.Add(student);  // bu kısımda interface kullanıldı 
+            manager.Add(customer); // bu kısımda interface kullanıldı
+            manager.Add(new Worker { Id = 1, FirtName = "Abdullah3", LastName = "Boz", Departmant = "Project Asistant" }); ;
+
+        }
+        interface IPerson
+        {
+            // Her biri implement edilmelidir.
+            int Id { get; set; }
+            string FirtName { get; set; }
+            string LastName { get; set; }
+        }
+        class Customer : IPerson
+        {
+            public int Id { get; set; }
+            public string FirtName { get; set; }
+            public string LastName { get; set; }
+            // müşteriye ait ayrıca özellik
+            public string Address { get; set; }
+        }
+        class Student : IPerson
+        {
+            public int Id { get; set; }
+            public string FirtName { get; set; }
+            public string LastName { get; set; }
+            // öğrenciye ait ayrıca özellik.
+            public string Departmant { get; set; }
+
+        }
+        class Worker : IPerson
+        {
+            public int Id { get; set; }
+            public string FirtName { get; set; }
+            public string LastName { get; set; }
+            // öğrenciye ait ayrıca özellik.
+            public string Departmant { get; set; }
+
+        }
+        class PersonManager
+        {
+            //nesne de parametre olabilir.
+            public void Add(IPerson person) // Parametre olarak bir ekleme yapacağım bana parametre olarak bir müşteri nesnesi ver.
+            {
+                Console.WriteLine(person.FirtName); 
+                // Burada person olarak inter face kullandığımız için yukarıda manager.Add(student yada customer ekleyebiliyoruz.)
+            }
+            // Katmanlar arası geçiş yaparken ve servis implementinde kullanılır.
+        }
+    }
+}
