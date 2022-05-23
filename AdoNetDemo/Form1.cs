77a05ba8be3609dceedc6181c97c3f19a362bc7d
AbdullahBoz <abdullahboz@outlook.com>
2022-05-23 05:07:07 +0300

    AdoNetDemo
    Database CRUD processes

---
+++ AdoNetDemo/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNetDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ProductDal _productDal = new ProductDal(); // product dal'ı çağırıyoruz.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            LoadProducts(); // metotu çağırdığımız için form açılır açılmaz geliyor.
+        }
+        private void LoadProducts() // veritabanı dgw'e geliyor.
+        {
+            dgwProducts.DataSource = _productDal.GetAll(); // Datasource bizden bir obje istiyor. bu nedenle ^
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            _productDal.Add(new Product 
+            {
+                Name=tbxName.Text,
+                UnitPrice=Convert.ToDecimal(tbxUnitPrice.Text),
+                StockAmount=Convert.ToInt32(tbxStockAmount.Text)
+            });
+            LoadProducts(); // Ekle butonuna bastığımızda da geliyor.
+            MessageBox.Show("Product Added!");
+        }
+
+        private void dgwProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tbxUpdateName.Text = dgwProducts.CurrentRow.Cells[1].Value.ToString();
+            tbxUpdateUnitPrice.Text = dgwProducts.CurrentRow.Cells[2].Value.ToString();
+            tbxStockAmountUpdate.Text = dgwProducts.CurrentRow.Cells[3].Value.ToString();//seçili olan satırın 1. hücresini değiştiricez 1. hücrede Id var.
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {          
+            Product product = new Product
+            {
+                Id = Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value), //Id'yi gridden alıyoruz kullanıcı değiştirmesin diye.
+                Name = tbxUpdateName.Text,
+                UnitPrice = Convert.ToDecimal(tbxUpdateUnitPrice.Text),
+                StockAmount = Convert.ToInt32(tbxStockAmountUpdate.Text)
+            };
+            _productDal.Update(product);
+            LoadProducts();
+            MessageBox.Show("Updated!");
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value);
+            _productDal.Delete(id);
+            LoadProducts();
+            MessageBox.Show("Deleted!");
+            
+        }
+    }
+}
