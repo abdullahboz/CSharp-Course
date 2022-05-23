e1e105c05ca6a3c3e0e5aadeda0e0b6d20f8bd3a
AbdullahBoz <abdullahboz@outlook.com>
2022-05-23 05:03:06 +0300

    Acces Modifiers
    Erişim belirleyiciler

---
+++ AccesModifiersDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccessModifiers; // <--- Burada ekledik 
+
+namespace AccesModifiersDemo
+{
+    /*Başka bir projedeki sınıfı kullanmak istiyorsak using yapmamız lazım ve diğer projedeki class ı public yapmamız gerekir.
+     * 
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Course couse = new Course();
+        }
+    }
+}
