dd9820d2845d7bc0e034669082531a78494925ef
AbdullahBoz <abdullahboz@outlook.com>
2022-05-26 02:30:07 +0300

    Event
    eventlerin kullanımı temel olarak anlatılıyor.

---
+++ Events/Product.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public delegate void StockControl();
+    public class Product
+    {
+        private int _stock;
+        public Product(int stock)
+            // Veritabanından çektiğimizi varsayalım
+            // bir stock gönderiyorum onuda _stock'u stock diye tanımlıyorum 
+            // injection yapmış oluyoruz.
+            // _stock'u kullanıyoruz.
+        {
+            _stock = stock;
+        }
+
+        public event StockControl StockControlEvent;
+        // event tanımlıyoruz. Sonuna event yazıyoruz.
+        public string ProductName { get; set; }
+
+       
+        public int Stock {
+            get
+            {
+                return _stock;
+            }
+            set
+            {
+                _stock=value;
+                if (value <= 15 && StockControlEvent!=null)
+                    // stock 15'ten az kaldıysa ve
+                    // stock nesnesine abone olunmuşsa (değer null olmamalı)
+                {
+                    StockControlEvent();
+                    // Eventi tetikliyoruz.
+
+                    /* Ürünü hem okuyabiliyoruz.
+                     * Hem set edebiliyoruz.
+                     * Aynı zaman koşul ekleyerek
+                     * Stock 15'ten eşit veya küçük ise 
+                     * ve StockControlEvent'imiz null değil yani ürüne abone olunmuşsa koşulumuzu çalıştırıyoruz.
+                    */
+                }
+
+            } 
+        }
+        // Stock'u get ve set ediyoruz. Bu çerçevede bir event varsa bu seti genişletmemiz gerekiyor.
+
+        public void Sell(int amount)
+        {
+            Stock -= amount;
+            // stocktan kaçtane satış yaptıysak yani 50 ise 50=50-10 yani 40 kalacak.
+            Console.WriteLine("{1} Stock amount : {0}", Stock,ProductName);
+        }
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    internal class Program
+    {
+        // eventleri programda bir hareket olduğunda dönüt almak için kullanıyoruz.
+        // büyük projelerde onlarca if kullanmak yerine eventleri kullanmak daha mantıklıdır.
+        // eventler aslında bir delegedir.
+
+        /*
+         * FormApp'te form'a bir button ekledikten sonra
+         * çift tıkladığımızda bir button clik eventi oluşturuyoruz mutlaka görmüşsünüzdür.
+         * designer kısmına gidip baktığımızda eventi kayıt ediyor += ile bakabilirsiniz.
+         */
+        static void Main(string[] args)
+        {
+            Product harddisk = new Product(50);
+            harddisk.ProductName = "Hard Disk";
+
+            Product gsm = new Product(50);
+            gsm.ProductName = "GSM";
+            gsm.StockControlEvent += Gsm_StockControlEvent;
+            for (int i = 0; i < 10; i++)
+            {
+                harddisk.Sell(10);
+                gsm.Sell(10);
+                Console.ReadLine();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Gsm_StockControlEvent()
+        {
+            Console.WriteLine("Gsm about to finish!");
+        }
+    }
+}
